08c7036711276bdd0f8166173711d2231dd5ccdb
NoriX <versatility.95@mail.ru>
2019-03-10 19:13:47 +0300

    Adding many different props to component

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import faker from 'faker';
 
 const CommentDetail = (props) => {
     return (
         <div className="comment">
             <a href="/" className="avatar">
-                <img alt="avatar" src={faker.image.avatar()}></img>
+                <img alt="avatar" src={props.avatar}></img>
             </a>
             <div className="content">
                 <a href="/" className="author">{props.author}</a>
                 <div className="metadata">
-                    <span className="date">Today at 6:00PM</span>
+                    <span className="date">{props.timeAgo}</span>
                 </div>
-                <div className="text">Lorem ipsum dolor sit.</div>
+                <div className="text">{props.text}</div>
             </div>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" />
-            <CommentDetail author="Alex" />
-            <CommentDetail author="Jane" />
+            <CommentDetail author="Sam" timeAgo="Today at 3:00PM" text="Lorem ipsum dolor sit." avatar={faker.image.avatar()}/>
+            <CommentDetail author="Alex" timeAgo="Today at 4:00PM" text="Lorem ipsum dolor." avatar={faker.image.avatar()}/>
+            <CommentDetail author="Jane" timeAgo="Today at 5:00PM" text="Lorem ipsum." avatar={faker.image.avatar()}/>
         </div>
     );
 };
