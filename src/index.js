6fd5e7d261fec48247ac05733eb74a49978b1aeb
NoriX <versatility.95@mail.ru>
2019-03-10 19:34:39 +0300

    Reuse of component (Component as Property in another component)

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" timeAgo="Today at 3:00PM" text="Lorem ipsum dolor sit." avatar={faker.image.avatar()}/>
-            <CommentDetail author="Alex" timeAgo="Today at 4:00PM" text="Lorem ipsum dolor." avatar={faker.image.avatar()}/>
-            <CommentDetail author="Jane" timeAgo="Today at 5:00PM" text="Lorem ipsum." avatar={faker.image.avatar()}/>
+            <ApprovalCard>
+                <CommentDetail author="Sam" timeAgo="Today at 3:00PM" text="Lorem ipsum dolor sit." avatar={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Alex" timeAgo="Today at 4:00PM" text="Lorem ipsum dolor." avatar={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Jane" timeAgo="Today at 5:00PM" text="Lorem ipsum." avatar={faker.image.avatar()} />
+            </ApprovalCard>
         </div>
     );
 };
