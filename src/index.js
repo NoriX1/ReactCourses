defc65df8fa4ca305ec05f41061c1313ca4eb4c7
NoriX <versatility.95@mail.ru>
2019-03-11 19:45:56 +0300

    Exercises

--- src/index.js
+++ src/index.js
@@ -2,33 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
+import UserForm from './UserForm';
+import Clock from './Clock';
 
-class App extends React.Component{
-    state = {lat: null, errorMessage: ''};
+class App extends React.Component {
+    state = { lat: null, errorMessage: '' };
 
-    componentDidMount(){
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
                 this.setState({ lat: position.coords.latitude });
             },
             (err) => {
-                this.setState({errorMessage: err.message});
+                this.setState({ errorMessage: err.message });
             }
         );
     }
 
-    renderContent(){
-        if (this.state.errorMessage && !this.state.lat){
-            return <div>Error: {this.state.errorMessage}</div>;
+    renderContent() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return (
+                <div>
+                    <div>Error: {this.state.errorMessage}</div>
+                    <UserForm/>
+                    <Clock/>
+                </div>
+            );
         }
-        if (!this.state.errorMessage && this.state.lat){
-            return <SeasonDisplay lat={this.state.lat}/>;
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />;
         }
-        
-        return <Spinner message="Please accept location request"/>;
+
+        return <Spinner message="Please accept location request" />;
     }
 
-    render(){
+    render() {
         return (
             <div className="border red">{this.renderContent()}</div>
         );
