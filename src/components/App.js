b794ae7a64533e0f3bf07e711aa9636e895a2be5
NoriX <versatility.95@mail.ru>
2019-03-12 19:53:39 +0300

    Updating state property

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,23 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    async onSearchSubmit(term) {
+    state = { images: [] };
+    onSearchSubmit = async (term) => {
         const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term },
             headers: {
                 Authorization: 'Client-ID 1971b37bb3960e5ec3a48b55c97551a544cc0efd4dd08416490990e7a157591a'
             }
-        })
-        console.log(response.data.results);
+        });
+
+        this.setState({images: response.data.results});
     }
 
     render() {
         return (
             <div className="ui container" style={{ marginTop: '10px' }}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found {this.state.images.length} images
             </div>
         );
     }
