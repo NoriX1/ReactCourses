3ae4549656f717c383a074c0ff46249006ce117a
NoriX <versatility.95@mail.ru>
2019-03-12 19:38:12 +0300

    Sending Request and Receiving Response with async await

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,14 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    onSearchSubmit(term) {
-        axios.get('https://api.unsplash.com/search/photos', {
+    async onSearchSubmit(term) {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term },
             headers: {
                 Authorization: 'Client-ID 1971b37bb3960e5ec3a48b55c97551a544cc0efd4dd08416490990e7a157591a'
             }
-        }).then((response) => {
-            console.log(response.data.results)
-        });
+        })
+        console.log(response.data.results);
     }
 
     render() {
