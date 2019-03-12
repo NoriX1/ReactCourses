26b483de06b01367780f6674deb88ae6af8cf129
NoriX <versatility.95@mail.ru>
2019-03-12 19:36:10 +0300

    Sending Request and Receiving Response

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
     onSearchSubmit(term) {
-        console.log(term);
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID 1971b37bb3960e5ec3a48b55c97551a544cc0efd4dd08416490990e7a157591a'
+            }
+        }).then((response) => {
+            console.log(response.data.results)
+        });
     }
 
     render() {
