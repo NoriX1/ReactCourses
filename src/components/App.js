fb44a035fe3d018af0c829b2c5885242668f1861
NoriX <versatility.95@mail.ru>
2019-03-13 18:42:54 +0300

    VideoList Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
+import VideoList from './VideoList';
 
 class App extends React.Component {
-    state = {}
-    
+    state = { videos: [] }
+
     onSearchSubmit = async term => {
-        const response = await youtube.get('/search',{
-            params: {q: term}
+        const response = await youtube.get('/search', {
+            params: { q: term }
         });
-        console.log(response)
+        this.setState({ videos: response.data.items });
     }
 
     render() {
         return (
             <div className="ui container">
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                <VideoList videos={this.state.videos}/>
             </div>
         );
     }
---
+++ src/components/VideoList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+
+const VideoList = ({videos}) => {
+    return <div className="">Found {videos.length} videos.</div>
+};
+
+
+export default VideoList
