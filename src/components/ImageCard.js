6a9a6dc86e4432669ea678604c9735fab1d1f769
NoriX <versatility.95@mail.ru>
2019-03-13 16:15:48 +0300

    Receiving 0 Height of Image Error

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,25 @@
 import React from 'react';
 
 class ImageCard extends React.Component{
+    constructor(props){
+        super(props);
 
+        //Ref ипользуется для получения доступа к конкретному элементу React
+        //Заменяет собой получение элемента через document.querySelector()
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount(){
+        console.log(this.imageRef);
+        //Из-за того, что в данный момент изображение ещё не загрузилось
+        //Вместо реальной высоты изображения мы увидим 0
+        console.log(this.imageRef.current.clientHeight);
+    }
     render(){
         const {description, urls} = this.props.image;
         return (
             <div>
-                <img src={urls.regular} alt={description}/>
+                <img ref={this.imageRef} src={urls.regular} alt={description}/>
             </div>
         );
     }
