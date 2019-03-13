e3be194451ead75e27157d00ba7661980522ff73
NoriX <versatility.95@mail.ru>
2019-03-13 18:29:54 +0300

    SearchBar and APIs

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
+
+class App extends React.Component {
+    state = {}
+    
+    onSearchSubmit = async term => {
+        const response = await youtube.get('/search',{
+            params: {q: term}
+        });
+        console.log(response)
+    }
+
+    render() {
+        return (
+            <div className="ui container">
+                <SearchBar onSubmit={this.onSearchSubmit} />
+            </div>
+        );
+    }
+}
+
+export default App;
