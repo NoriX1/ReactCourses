e4fb38ebd4c4e4431ca0ced1711858aa29db2db1
NoriX <versatility.95@mail.ru>
2019-03-13 19:20:54 +0300

    VideoList and VideoItem Styling

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import VideoItem from './VideoItem';
 
 
 
-const VideoList = ({videos}) => {
-    return <div className="">Found {videos.length} videos.</div>
+const VideoList = ({ videos }) => {
+    const renderedList = videos.map((video) => {
+        return <VideoItem key={video.id.videoId} video={video} />;
+    });
+    return <div className="ui relaxed divided list">{renderedList}</div>
 };
 
 
